4fb0d354e0bdfd04dc8a803af414a5f0c8dff206
Lee Siwoo <137245969+2cu-1001@users.noreply.github.com>
2024-12-30 07:13:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from dataset import DataSet
 from path import Path, Node
 from utils import make_group_nodes_order_optimal
@@ -6,17 +7,7 @@ from pathVisualizer import visualize_path
 import utils
 
 
-def main():
-    #Simulated Annealing Setting
-    T = 150000
-    T_final = 0.01
-    delta1 = 0.9995
-    delta2 = 0.99
-    const = 5000
-    max_iter = 1000000
-    target_cost = 2000
-    #Simulated Annealing Setting
-    
+def main(T, T_final, delta1, delta2, const, max_iter, target_cost):
     data_path = "./data/data.csv"
     dataSet = DataSet(data_path)  
     nodes = dataSet.make_datas2nodes()
@@ -41,4 +32,23 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--T", type=float, default=150000, help="Initial temperature.")
+    parser.add_argument("--T_final", type=float, default=0.01, help="Final temperature.")
+    parser.add_argument("--delta1", type=float, default=0.9995, help="Temperature decay rate1")
+    parser.add_argument("--delta2", type=float, default=0.99, help="Temperature decay rate2")
+    parser.add_argument("--const", type=int, default=5000, help="Constant factor for acceptance probability.")
+    parser.add_argument("--max_iter", type=int, default=1000000, help="Maximum number of iterations.")
+    parser.add_argument("--target_cost", type=float, default=2000, help="Target cost to achieve.")
+
+    args = parser.parse_args()
+
+    main(
+        T=args.T,
+        T_final=args.T_final,
+        delta1=args.delta1,
+        delta2=args.delta2,
+        const=args.const,
+        max_iter=args.max_iter,
+        target_cost=args.target_cost
+    )
