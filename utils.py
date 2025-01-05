f438708db5f33360a2c96a63d15b44088598627f
Lee Siwoo <137245969+2cu-1001@users.noreply.github.com>
2025-01-06 07:17:02 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import math
 from itertools import permutations
+from functools import lru_cache
 
 
+@lru_cache(maxsize=None)
 def calc_dist(x1, y1, x2, y2):
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
